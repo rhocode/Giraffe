6e23418dd89eddfa3059e73fe0cc3bf8788ee7fe
Alex <aafu@ucdavis.edu>
2019-06-08 12:35:55 -0400

    Add secrets

--- src/common/firebase/firebaseSecrets.js
+++
@@ -1,9 +0,0 @@
-export const firebaseSecrets = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
-};
