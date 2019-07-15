ff82c56f1a428106e7fd777ed4cf9d1318f44f44
Adam <adam@thinkaliker.com>
2019-07-14 22:11:53 -0700

    Add graph fidelity checkbox to settings dialog

--- src/apps/Graph/components/GraphSettingsButton.jsx
+++ src/apps/Graph/components/GraphSettingsButton.jsx
@@ -1,26 +1,56 @@
 import { withStyles } from '@material-ui/core';
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
 import SettingsIcon from '@material-ui/icons/Settings';
 
 import GraphAppBarButton from './GraphAppBarButton';
+import { setGraphFidelity } from '../../../redux/actions/Graph/graphActions';
 
 const styles = theme => ({});
 
-class GraphSettingsButton extends Component {
-  render() {
-    const { classes } = this.props;
+function GraphSettingsButton(props) {
+  const { classes, graphFidelity } = props;
 
-    return (
-      <GraphAppBarButton label="Settings" icon={<SettingsIcon />}>
-        hi
-      </GraphAppBarButton>
-    );
+  function handleChange(event, newValue) {
+    props.setGraphFidelity(newValue ? 'high' : 'low');
   }
+
+  return (
+    <GraphAppBarButton label="Settings" icon={<SettingsIcon />}>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={graphFidelity === 'high'}
+              onChange={handleChange}
+              value={graphFidelity}
+              color="primary"
+            />
+          }
+          label="High Fidelity Graph"
+        />
+      </FormGroup>
+    </GraphAppBarButton>
+  );
 }
 
-const mapStateToProps = () => ({});
+function mapStateToProps(state) {
+  return {
+    graphFidelity: state.graphReducer.graphFidelity
+  };
+}
 
-export default connect(mapStateToProps)(
-  withStyles(styles)(GraphSettingsButton)
-);
+function mapDispatchToProps(dispatch) {
+  return {
+    setGraphFidelity: data => dispatch(setGraphFidelity(data))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(GraphSettingsButton));
