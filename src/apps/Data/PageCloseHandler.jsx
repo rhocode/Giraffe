1b3980e64e2b067088732ebf592ca99536f6108d
Alex <aafu@ucdavis.edu>
2019-08-30 03:16:19 -0400

    Add other editors in

--- src/apps/Data/PageCloseHandler.jsx
+++ src/apps/Data/PageCloseHandler.jsx
@@ -5,24 +5,28 @@ function PageCloseHandler(props) {
   React.useEffect(() => {
     const handleBeforeUnload = event => {
       const check =
-        props.Item &&
-        props.Item.rows.every(entry => !entry.local) &&
-        props.MachineClass &&
-        props.MachineClass.rows.every(entry => !entry.local) &&
-        props.Recipe &&
-        props.Recipe.rows.every(entry => !entry.local);
+        props.Item === props.Item_original &&
+        props.MachineClass === props.MachineClass_original &&
+        props.Recipe === props.Recipe_original;
 
       console.error(check);
 
       if (!check) {
         event.preventDefault();
-        return 'Are you sure to leave this page?';
+        event.returnValue = 'Do you really want to leave this page?';
       }
     };
     window.addEventListener('beforeunload', handleBeforeUnload);
 
     return () => window.removeEventListener('beforeunload', handleBeforeUnload);
-  }, [props.Item, props.MachineClass, props.Recipe]);
+  }, [
+    props.Item,
+    props.Item_original,
+    props.MachineClass,
+    props.MachineClass_original,
+    props.Recipe,
+    props.Recipe_original
+  ]);
 
   return <div />;
 }
@@ -30,7 +34,10 @@ function PageCloseHandler(props) {
 const mapStateToProps = state => ({
   Item: state.dataReducer.Item,
   MachineClass: state.dataReducer.MachineClass,
-  Recipe: state.dataReducer.Recipe
+  Recipe: state.dataReducer.Recipe,
+  Item_original: state.dataReducer.Item_original,
+  MachineClass_original: state.dataReducer.MachineClass_original,
+  Recipe_original: state.dataReducer.Recipe_original
 });
 
 const mapDispatchToProps = () => ({});
