7a05eca5720dbd1c7cd8ce2b2ed686444bf6731b
Alex <aafu@ucdavis.edu>
2019-09-02 04:40:11 -0400

    Add png for the larger image sizes

---
+++ src/apps/Graph/libraries/SGLib/react/plugins/zoomFunction.js
@@ -0,0 +1,28 @@
+import * as d3 from 'd3';
+
+const zoomPlugin = (
+  graphData,
+  graphFidelity,
+  mouseMode,
+  setTransform,
+  transform,
+  simulationUpdate
+) => {
+  if (mouseMode !== 'move' && mouseMode !== 'add') {
+    return;
+  }
+
+  const localTransform = d3.event.transform;
+
+  setTransform(localTransform);
+
+  if (graphFidelity !== 'low' && localTransform.k !== transform.k) {
+    graphData.nodes.forEach(node => {
+      node.preRender(localTransform);
+    });
+  }
+
+  simulationUpdate(localTransform);
+};
+
+export default zoomPlugin;
