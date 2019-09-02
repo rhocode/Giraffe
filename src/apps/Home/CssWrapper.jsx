5f64e5164e169a721c127afc293228792dfd6b7e
Alex <aafu@ucdavis.edu>
2019-09-01 20:14:41 -0400

    Fix LightHouse score

--- src/apps/Home/CssWrapper.jsx
+++ src/apps/Home/CssWrapper.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class CssWrapper extends Component {
   componentWillMount() {
-    console.error('Added app.css here');
     require('./App.css');
   }
 
