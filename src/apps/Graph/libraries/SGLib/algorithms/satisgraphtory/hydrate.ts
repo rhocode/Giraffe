3de2cea023536f26fc4e834ba0a1b51d0390cb4b
Alex <aafu@ucdavis.edu>
2019-09-05 06:26:01 -0400

    Add the initial loading of the data

--- src/apps/Graph/libraries/SGLib/algorithms/satisgraphtory/hydrate.ts
+++ src/apps/Graph/libraries/SGLib/algorithms/satisgraphtory/hydrate.ts
@@ -1,9 +1,59 @@
-const hydrate = (deserializedData: any, translate: any = (a: string) => a) => {
-  // const nodes = deserializedData.nodes.map((nodeData: any) => {
-  //     console.log(nodeData);
-  // })
-  // const edges = fullyDecoded.edges.map()
-  // console.log(fullyDecoded);
+import MachineNode from '../../datatypes/graph/graphNode';
+import { getPlaceableMachineClasses } from '../../../../graphql/queries';
+import { GraphEdge } from '../../datatypes/graph/graphEdge';
+
+const hydrate = (
+  deserializedData: any,
+  translate: any = (a: string) => a,
+  transform: any,
+  callback: any
+) => {
+  const nodes = deserializedData.nodes;
+  const edges = deserializedData.edges;
+  getPlaceableMachineClasses().then((classes: any) => {
+    const mapping: any = {};
+    Object.values(classes).forEach((value: any) => {
+      mapping[value.name] = value;
+    });
+
+    const nodeMapping: any = {};
+
+    const hydratedNodes = nodes.map((node: any) => {
+      const selectedMachine = {
+        recipe: node.recipe,
+        class: mapping[node.machineClass],
+        tier: node.tier
+      };
+
+      const newNode = new MachineNode(
+        selectedMachine,
+        node.overclock,
+        node.fx,
+        node.fy,
+        false,
+        translate,
+        transform,
+        node.id
+      );
+      nodeMapping[node.id] = newNode;
+      return newNode;
+    });
+
+    const hydratedEdges = edges.map((edge: any) => {
+      return new GraphEdge(
+        nodeMapping[edge.sourceNodeId],
+        nodeMapping[edge.targetNodeId],
+        edge.tier,
+        true,
+        edge
+      );
+    });
+
+    callback({
+      nodes: hydratedNodes,
+      edges: hydratedEdges
+    });
+  });
 };
 
 export default hydrate;
