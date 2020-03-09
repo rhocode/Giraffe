307573202af3183cff1d8b759394298ada82f608
Alex <aafu@ucdavis.edu>
2020-03-09 03:03:33 -0400

    Add in algorithm for recursive path

--- src/v3/apps/DataV3/DataApp.jsx
+++ src/v3/apps/DataV3/DataApp.jsx
@@ -2,11 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
 import initRuntime from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/initRuntime';
-import { GET_RECIPES_BY_MACHINE } from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/graphql/queries';
-import gqlClient from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/graphql/gqlClient';
-import dataParser from '../GraphV3/libraries/SatisGraphtoryLib/sourcetools/dataParser';
-
-const item = require('development/Docs.json');
+// import { GET_RECIPES_BY_MACHINE } from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/graphql/queries';
+// import gqlClient from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/graphql/gqlClient';
+// import dataParser from '../GraphV3/libraries/SatisGraphtoryLib/sourcetools/dataParser';
+//
+// const item = require('development/Docs.json');
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,7 +28,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function DataApp(props) {
+function DataApp() {
   const classes = useStyles();
 
   React.useEffect(() => {
@@ -37,17 +37,17 @@ function DataApp(props) {
     initRuntime();
   }, []);
 
-  console.log('@@#fff@');
-  const client = gqlClient();
-  client
-    .query({
-      query: GET_RECIPES_BY_MACHINE
-    })
-    .then(response => {
-      console.log(response);
-    });
+  // console.log('@@#fff@');
+  // const client = gqlClient();
+  // client
+  //   .query({
+  //     query: GET_RECIPES_BY_MACHINE
+  //   })
+  //   .then(response => {
+  //     console.log(response);
+  //   });
 
-  return <div className={classes.root}></div>;
+  return <div className={classes.root} />;
 }
 
 export default DataApp;
--- src/v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/runtime/bruteForceChainGeneration.ts
+++ src/v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/runtime/bruteForceChainGeneration.ts
@@ -10,6 +10,8 @@ function eqSet(as: any, bs: any) {
   return as.size === bs.size && all(isIn(bs), as);
 }
 
+console.log(eqSet);
+
 function all(pred: any, as: any) {
   for (let a of as) if (!pred(a)) return false;
   return true;
@@ -19,6 +21,47 @@ function isIn(as: any) {
   return function(a: any) {
     return as.has(a);
   };
+}
+
+function calculateDuplicates(matrix: any) {
+  const sortedMatrix: any = {};
+  matrix.forEach((element: any) => {
+    const allNames: string[] = [];
+    element.cost.forEach((value: any, key: string) => {
+      allNames.push(key);
+    });
+    const joinedName = allNames.sort().join('');
+    if (sortedMatrix[joinedName] !== undefined) {
+      const fetchedMatrices = sortedMatrix[joinedName];
+
+      const recipeToCheck = element.cost;
+
+      const replacedMatrices = new Set(
+        fetchedMatrices.filter((possibleCostMatrix: any) => {
+          const isLess: any[] = [];
+          possibleCostMatrix.cost.forEach((value: any, key: string) => {
+            isLess.push(math.smaller(recipeToCheck.get(key), value));
+          });
+
+          console.log(isLess);
+
+          return isLess.every((bool: boolean) => bool);
+        })
+      );
+      if (replacedMatrices.size) {
+        sortedMatrix[joinedName] = [
+          ...fetchedMatrices.filter((item: any) => !replacedMatrices.has(item)),
+          element
+        ];
+      } else {
+        sortedMatrix[joinedName] = [...fetchedMatrices, element];
+      }
+    } else {
+      sortedMatrix[joinedName] = [element];
+    }
+  });
+
+  return Object.values(sortedMatrix).flat(1);
 }
 
 export default function bruteForceChainGeneration(data: any) {
@@ -35,9 +78,6 @@ export default function bruteForceChainGeneration(data: any) {
   );
 
   const recipesUsed = data.recipe;
-  // .filter((item: any) => {
-  //   return item.name.indexOf('Alternate') === -1;
-  // });
 
   const ingredientsToRecipe = new Map();
   const productsToRecipe = new Map();
@@ -83,119 +123,334 @@ export default function bruteForceChainGeneration(data: any) {
     ...extractorProducedResources
   ]);
 
-  const target = 'IronRod';
-
-  // const recursivePath = (item: string, currentPath: any) => {
-  //
-  //   const traversal: string[] = [...(productsToRecipe.get(item) ?? [])].map(item => item.name);
-  //
-  //   const visitedRecipes = new Set();
-  //   const parent = new Map();
-  //
-  //   while(traversal.length) {
-  //     const current = traversal.shift()!;
-  //     if (visitedRecipes.has(current)) {
-  //        continue;
-  //     }
-  //
-  //     console.log("Visiting", current);
-  //     visitedRecipes.add(current);
-  //
-  //     const constituents = nameToRecipe.get(current)!.ingredients;
-  //
-  //     constituents.forEach((ingredient: any) => {
-  //
-  //       const resource = ingredient.resource;
-  //       console.log("  Ingredient: ", resource)
-  //       const possibleRecipes = [...(productsToRecipe.get(resource) ?? [])].map(item => item.name);
-  //       console.log("  Recipes: ", productsToRecipe.get(resource));
-  //
-  //       possibleRecipes.forEach((subRecipe: string) => {
-  //         if (!parent.has(subRecipe)) {
-  //           parent.set(subRecipe, current);
-  //           traversal.push(subRecipe);
-  //         }
-  //       })
-  //     });
-  //   }
-  //   console.log(parent);
-  // };
-
-  const visitedRecipes = new Set();
+  const target = 'Wire';
 
   const resolvedRecipe: any = new Map();
 
-  const recursivePath = (
-    item: string,
-    quantity: number,
-    visitedSet: any = new Set()
-  ) => {
-    if (coreResources.has(item)) {
-      //TODO: fix item and do basic math to get the quantity
-      return {
-        recipe: 'BASIC',
-        ingredientMap: null,
-        currentCost: [[{ qty: 1, item }]]
+  const cache: any = {};
+  const recipeCache: any = {};
+
+  const recursivePathGenerator = (target: string, rate: any) => {
+    if (cache[target] === undefined) {
+      const resolvePath = (
+        target: string,
+        rate: any,
+        visitedSet: any = new Set()
+      ): any[] => {
+        if (coreResources.has(target)) {
+          const recipe = target;
+          return [
+            {
+              name: recipe,
+              recipes: [recipe],
+              paths: [[]],
+              cost: new Map([[target, rate]])
+            }
+          ];
+        } else {
+          const possibleRecipes = [...(productsToRecipe.get(target) ?? [])].map(
+            item => item.name
+          );
+
+          const choices = possibleRecipes
+            .filter((item: any) => !visitedSet.has(item))
+            .map((recipe: any) => {
+              if (recipeCache[recipe] === undefined) {
+                const amendedVisitedSet = new Set([...visitedSet, recipe]);
+
+                const fetchedRecipe = nameToRecipe.get(recipe);
+                const recipeIngredients = fetchedRecipe.ingredients;
+                const duration = fetchedRecipe.manufacturingDuration;
+
+                const actual = nameToRecipe
+                  .get(recipe)
+                  .product.filter((prod: any) => prod.resource === target);
+
+                if (actual.length !== 1) {
+                  throw new Error(
+                    'Something wrong with recipe, it has multiple actual resources'
+                  );
+                }
+
+                const requiredQuantity = actual[0]!.amount;
+
+                let requestedRate = rate;
+                if (typeof rate === 'number') {
+                  requestedRate = math.fraction(rate, 1);
+                }
+
+                const adjustedRecipeOutputQuantity = math.divide(
+                  requiredQuantity,
+                  duration
+                );
+
+                let multiplier = math.divide(
+                  requestedRate,
+                  adjustedRecipeOutputQuantity
+                );
+
+                const initialResult = new Map();
+
+                recipeIngredients.forEach((ing: any) => {
+                  const recipeRate = math.fraction(ing.amount, duration);
+                  initialResult.set(
+                    ing.resource,
+                    resolvePath(
+                      ing.resource,
+                      math.multiply(multiplier, recipeRate),
+                      amendedVisitedSet
+                    )
+                  );
+                });
+
+                let matrix: any = [];
+
+                // For each resource there is N ways to make it.
+                initialResult.forEach(
+                  (resourceCosts: any, resourceName: string) => {
+                    if (matrix.length === 0) {
+                      resourceCosts.forEach((constituent: any) => {
+                        matrix.push({
+                          name: recipe,
+                          recipes: [constituent.name],
+                          paths: [constituent.recipes],
+                          cost: constituent.cost
+                        });
+                      });
+                    } else {
+                      let newMatrix: any = [];
+                      resourceCosts.forEach((constituent: any) => {
+                        matrix.forEach((matrixElement: any) => {
+                          const newRecipes = [
+                            ...matrixElement.recipes,
+                            constituent.name
+                          ];
+                          const newPaths = [
+                            ...matrixElement.paths,
+                            constituent.recipes
+                          ];
+                          const totalNewCost = new Map();
+                          matrixElement.cost.forEach(
+                            (value: any, key: string) => {
+                              totalNewCost.set(key, value);
+                            }
+                          );
+
+                          constituent.cost.forEach(
+                            (value: any, key: string) => {
+                              const fetchedConstCost = totalNewCost.get(key);
+                              if (fetchedConstCost === undefined) {
+                                totalNewCost.set(key, value);
+                              } else {
+                                const newCost = math.add(
+                                  fetchedConstCost,
+                                  value
+                                );
+                                totalNewCost.set(key, newCost);
+                              }
+                            }
+                          );
+
+                          newMatrix.push({
+                            name: matrixElement.name,
+                            recipes: newRecipes,
+                            paths: newPaths,
+                            cost: totalNewCost
+                          });
+                        });
+                      });
+
+                      matrix = newMatrix;
+                    }
+                  }
+                );
+
+                recipeCache[recipe] = calculateDuplicates(matrix);
+              }
+              return recipeCache[recipe];
+            })
+            .flat(1)
+            .filter((item: any) => item !== null);
+          return calculateDuplicates(choices);
+        }
       };
+      cache[target] = resolvePath(target, rate);
     }
 
-    const possibleRecipes = [...(productsToRecipe.get(item) ?? [])].map(
-      item => item.name
-    );
-
-    const choices = possibleRecipes
-      .map((recipe: any) => {
-        if (visitedSet.has(recipe)) {
-          return null;
-        }
-        const amendedVisitedSet = new Set([...visitedSet, recipe]);
-        const recipeIngredients = nameToRecipe
-          .get(recipe)
-          .ingredients.map((item: any) => item.resource);
-
-        const actual = nameToRecipe
-          .get(recipe)
-          .product.filter((prod: any) => prod.resource === item);
-
-        if (actual.length !== 1) {
-          throw new Error(
-            'Something wrong with recipe, it has multiple actual resources'
-          );
-        }
-
-        const requiredQuantity = actual[0]!.amount;
-        console.log(requiredQuantity);
-
-        const multiplier = math.fraction(quantity, requiredQuantity);
-        console.log(multiplier);
-
-        let result;
-
-        if (resolvedRecipe.get(recipe) !== undefined) {
-          result = resolvedRecipe.get(recipe);
-        } else {
-          const initialResult = new Map();
-
-          recipeIngredients.forEach((ing: any) => {
-            initialResult.set(ing, recursivePath(ing, 2, amendedVisitedSet));
-          });
-
-          result = {
-            recipe: recipe,
-            currentCost: [[{ qty: 1, item }]],
-            ingredientMap: initialResult
-          };
-
-          resolvedRecipe.set(recipe, result);
-        }
-
-        return resolvedRecipe.get(recipe)!;
-      })
-      .filter((item: any) => item !== null);
-
-    return choices[0];
+    return cache[target];
   };
 
-  const choiceMap = recursivePath(target, 1);
-  console.log(choiceMap);
+  const choiceMap = recursivePathGenerator(target, 1);
+  console.log(
+    choiceMap.map((item: any) => {
+      const ret = [];
+      for (let entry of item.cost) {
+        ret.push([entry[0], entry[1].n / entry[1].d]);
+      }
+      return ret;
+    })
+  );
+
+  const recursivePath = (
+    item: string,
+    ratePerSecond: any,
+    visitedSet: any = new Set()
+  ) => {
+    if (cache[item] !== undefined) {
+      if (item === 'OreIron') {
+        throw new Error('Q#KNQK@#@Q');
+      }
+      return cache[item];
+    } else {
+      const resolveCacheValue = (
+        item: string,
+        ratePerSecond: any,
+        visitedSet: any = new Set()
+      ) => {
+        if (coreResources.has(item)) {
+          //TODO: fix item and do basic math to get the quantity
+          const recipe = item;
+          return [
+            {
+              name: recipe,
+              recipes: [recipe],
+              paths: [[]],
+              cost: new Map([[item, ratePerSecond]])
+            }
+          ];
+        }
+
+        const possibleRecipes = [...(productsToRecipe.get(item) ?? [])].map(
+          item => item.name
+        );
+
+        return possibleRecipes
+          .map((recipe: any) => {
+            if (visitedSet.has(recipe)) {
+              return null;
+            }
+            const amendedVisitedSet = new Set([...visitedSet, recipe]);
+
+            const fetchedRecipe = nameToRecipe.get(recipe);
+            const recipeIngredients = fetchedRecipe.ingredients;
+            const duration = fetchedRecipe.manufacturingDuration;
+
+            const actual = nameToRecipe
+              .get(recipe)
+              .product.filter((prod: any) => prod.resource === item);
+
+            if (actual.length !== 1) {
+              throw new Error(
+                'Something wrong with recipe, it has multiple actual resources'
+              );
+            }
+
+            const requiredQuantity = actual[0]!.amount;
+
+            let requestedRate = ratePerSecond;
+            if (typeof ratePerSecond === 'number') {
+              requestedRate = math.fraction(ratePerSecond, 1);
+            }
+
+            const adjustedRecipeOutputQuantity = math.divide(
+              requiredQuantity,
+              duration
+            );
+
+            let multiplier = math.divide(
+              requestedRate,
+              adjustedRecipeOutputQuantity
+            );
+            let result;
+
+            if (resolvedRecipe.get(recipe) !== undefined) {
+              result = resolvedRecipe.get(recipe);
+            } else {
+              const initialResult = new Map();
+
+              recipeIngredients.forEach((ing: any) => {
+                const recipeRate = math.fraction(ing.amount, duration);
+                initialResult.set(
+                  ing.resource,
+                  recursivePath(
+                    ing.resource,
+                    math.multiply(multiplier, recipeRate),
+                    amendedVisitedSet
+                  )
+                );
+              });
+
+              let matrix: any = [];
+
+              console.log(recipe, nameToRecipe.get(recipe));
+              console.log(initialResult);
+              // For each resource there is N ways to make it.
+              initialResult.forEach(
+                (resourceCosts: any, resourceName: string) => {
+                  if (matrix.length === 0) {
+                    resourceCosts.forEach((constituent: any) => {
+                      matrix.push({
+                        name: recipe,
+                        recipes: [constituent.name],
+                        paths: constituent.paths,
+                        cost: constituent.cost
+                      });
+                    });
+                  } else {
+                    let newMatrix: any = [];
+                    resourceCosts.forEach((constituent: any) => {
+                      matrix.forEach((matrixElement: any) => {
+                        const newRecipes = [
+                          ...constituent.recipes,
+                          ...matrixElement.recipes
+                        ];
+                        const newPaths = [
+                          ...constituent.paths,
+                          ...matrixElement.paths
+                        ];
+                        const totalNewCost = new Map();
+                        matrixElement.cost.forEach(
+                          (value: any, key: string) => {
+                            totalNewCost.set(key, value);
+                          }
+                        );
+
+                        constituent.cost.forEach((value: any, key: string) => {
+                          const fetchedConstCost = totalNewCost.get(key);
+                          if (fetchedConstCost === undefined) {
+                            totalNewCost.set(key, value);
+                          } else {
+                            const newCost = math.add(fetchedConstCost, value);
+                            totalNewCost.set(key, newCost);
+                          }
+                        });
+
+                        newMatrix.push({
+                          name: matrixElement.name,
+                          recipes: newRecipes,
+                          paths: newPaths,
+                          cost: totalNewCost
+                        });
+                      });
+                    });
+
+                    matrix = newMatrix;
+                  }
+                }
+              );
+
+              result = matrix;
+
+              resolvedRecipe.set(recipe, result);
+            }
+
+            return result;
+          })
+          .filter((item: any) => item !== null)
+          .flat(1);
+      };
+      cache[item] = resolveCacheValue(item, ratePerSecond, visitedSet);
+      console.log(cache[item]);
+    }
+  };
 }
