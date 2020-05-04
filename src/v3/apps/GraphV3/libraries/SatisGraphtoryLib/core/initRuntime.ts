801a5844a83cdf2b8cd35ebb6d86bedc8a5132f9
Alex <aafu@ucdavis.edu>
2020-05-03 20:37:21 -0400

    Bump version of multi-metagen

--- src/v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/initRuntime.ts
+++ src/v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/initRuntime.ts
@@ -4,19 +4,17 @@ import { getLatestSchemaName } from 'apps/Graph/libraries/SGLib/utils/getLatestS
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import worker from 'workerize-loader!./runtime/bruteForceChainGeneration';
-import {kiwiSolver} from "v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/runtime/kiwiSolver";
+import { kiwiSolver } from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/runtime/kiwiSolver';
 
 import rawEntities from './runtime/a.json';
 import rawRecipes from './runtime/b.json';
-import {solveFor} from "v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/runtime/solveFor";
+import { solveFor } from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/core/runtime/solveFor';
 
 const entities = rawEntities as any;
 const recipes = rawRecipes as any;
 
-
 const initRuntime = () => {
   memoizedProtoSpecLoader(getLatestSchemaName()).then((data: any) => {
-
     // const result = solveFor(recipes, entities, {
     //   targets: [{ slug: 'adaptive-control-unit', perMinute: 60 }],
     //   constraints: [],
@@ -31,7 +29,6 @@ const initRuntime = () => {
     // });
     // generateOrdering(data);
     // bruteForceChainGeneration(data);
-
 
     kiwiSolver(data);
   });
