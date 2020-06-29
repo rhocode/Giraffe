e11feff07382183e8ea21350a76ec158438f6794
Alexander Fu <aafu@ucdavis.edu>
2020-06-29 09:15:39 -0400

    Test edge compatibility

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'babel-polyfill';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
