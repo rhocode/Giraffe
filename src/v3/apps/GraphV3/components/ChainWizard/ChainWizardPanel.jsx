a42e3e42c8b46c27509c67f23b87c423cad67591
Alexander Fu <aafu@ucdavis.edu>
2020-07-08 01:07:12 -0400

    Third eslint test

--- src/v3/apps/GraphV3/components/ChainWizard/ChainWizardPanel.jsx
+++ src/v3/apps/GraphV3/components/ChainWizard/ChainWizardPanel.jsx
@@ -159,7 +159,7 @@ const updateAutoCalculate = (event, val) => {
 function ChainWizardPanel() {
   const classes = useStyles();
 
-  const demoVar = '';
+  const demoVar2 = '';
 
   const productsList = useStoreState(graphWizardStore, (s) => s.boxes);
 
