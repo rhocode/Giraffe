2b469bfbe8d485f72a58223ff38e1ef8b5fdeed8
Restyled.io <commits@restyled.io>
2020-08-20 22:06:40 +0000

    Restyled by clang-format

--- analyzer/analyze.js
+++ analyzer/analyze.js
@@ -1,9 +1,10 @@
 // script to enable webpack-bundle-analyzer
 process.env.NODE_ENV = 'production';
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
-const webpackConfigProd = require('react-scripts/config/webpack.config')('production');
+const BundleAnalyzerPlugin =
+    require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpackConfigProd =
+    require('react-scripts/config/webpack.config')('production');
 
 webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
 
