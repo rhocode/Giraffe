a62c372cf3a2579b98919273cb681060ba3ae96e
Alex Fu <aafu@ucdavis.edu>
2020-09-23 20:29:27 -0400

    Use the window to add event listeners instead

--- src/serviceWorkerCustom.js
+++ src/serviceWorkerCustom.js
@@ -26,65 +26,6 @@ const filesToCache = getAllImageFiles();
 
 const staticCacheName = 'satisgraphtory-image-cache';
 
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
-  event.waitUntil(
-    caches.open(staticCacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
-  );
-});
-
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(response => {
-          // TODO 5 - Respond with custom 404 page
-          return caches.open(staticCacheName).then(cache => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          });
-        });
-
-      }).catch(error => {
-
-      // TODO 6 - Respond with custom offline page
-
-    })
-  );
-});
-
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('activate', event => {
-  console.log('Activating new service worker...');
-
-  const cacheAllowlist = [staticCacheName];
-
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-
-          return Promise.resolve();
-        })
-      );
-    })
-  );
-});
-
 export function unregister(callback) {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
@@ -120,6 +61,64 @@ export function register(config) {
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
+
+    window.addEventListener('install', event => {
+      console.log('Attempting to install service worker and cache static assets');
+      event.waitUntil(
+        caches.open(staticCacheName)
+          .then(cache => {
+            return cache.addAll(filesToCache);
+          })
+      );
+    });
+
+
+    window.addEventListener('fetch', event => {
+      console.log('Fetch event for ', event.request.url);
+      event.respondWith(
+        caches.match(event.request)
+          .then(response => {
+            if (response) {
+              console.log('Found ', event.request.url, ' in cache');
+              return response;
+            }
+            console.log('Network request for ', event.request.url);
+            return fetch(event.request).then(response => {
+              // TODO 5 - Respond with custom 404 page
+              return caches.open(staticCacheName).then(cache => {
+                cache.put(event.request.url, response.clone());
+                return response;
+              });
+            });
+
+          }).catch(error => {
+
+          // TODO 6 - Respond with custom offline page
+
+        })
+      );
+    });
+
+
+    window.addEventListener('activate', event => {
+      console.log('Activating new service worker...');
+
+      const cacheAllowlist = [staticCacheName];
+
+      event.waitUntil(
+        caches.keys().then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              if (cacheAllowlist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+
+              return Promise.resolve();
+            })
+          );
+        })
+      );
+    });
 
     // We are not waiting for window's load event as it was for the create-react-app template
     // because we register a service worker in the React app that runs after window loaded
