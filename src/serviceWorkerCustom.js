7b8c000025031cd7e285ba13608c6dc7b02c313e
Alex Fu <aafu@ucdavis.edu>
2020-09-23 20:04:57 -0400

    Use cached images

--- src/serviceWorkerCustom.js
+++ src/serviceWorkerCustom.js
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+import {getAllImageFiles} from "v3/data/loaders/sgImageRepo";
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -19,6 +21,67 @@ const isLocalhost = Boolean(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
+
+const filesToCache = getAllImageFiles();
+
+const staticCacheName = 'satisgraphtory-image-cache';
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(response => {
+          // TODO 5 - Respond with custom 404 page
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+
+      }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheAllowlist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 export function unregister(callback) {
   if ('serviceWorker' in navigator) {
