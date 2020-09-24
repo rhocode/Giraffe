a0e5f0b2948638e31f5e99d02cc862fc39e1482c
Alex Fu <aafu@ucdavis.edu>
2020-09-23 21:05:49 -0400

    Add serviceWorker during register time

--- src/serviceWorkerCustom.js
+++ src/serviceWorkerCustom.js
@@ -87,72 +87,73 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
+  console.log("Trying to use serviceWorker directly");
+
+  navigator.serviceWorker.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+        .then(cache => {
+          return cache.addAll(filesToCache);
+        })
+    );
+  });
+
+
+
+  navigator.serviceWorker.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          if (response) {
+            console.log('Found ', event.request.url, ' in cache');
+            return response;
+          }
+          console.log('Network request for ', event.request.url);
+          return fetch(event.request).then(response => {
+            // TODO 5 - Respond with custom 404 page
+            return caches.open(staticCacheName).then(cache => {
+              cache.put(event.request.url, response.clone());
+              return response;
+            });
+          });
+
+        }).catch(error => {
+
+        // TODO 6 - Respond with custom offline page
+
+      })
+    );
+  });
+
+
+  navigator.serviceWorker.addEventListener('activate', event => {
+    console.log('Activating new service worker...');
+
+    const cacheAllowlist = [staticCacheName];
+
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheAllowlist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+
+            return Promise.resolve();
+          })
+        );
+      })
+    );
+  });
+
+
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
       console.log('Initing SW');
       if (registration) {
-
-        if (navigator.serviceWorker.controller) {
-          navigator.serviceWorker.controller.addEventListener('install', event => {
-            console.log('Attempting to install service worker and cache static assets');
-            event.waitUntil(
-              caches.open(staticCacheName)
-                .then(cache => {
-                  return cache.addAll(filesToCache);
-                })
-            );
-          });
-
-
-
-          navigator.serviceWorker.addEventListener('fetch', event => {
-            console.log('Fetch event for ', event.request.url);
-            event.respondWith(
-              caches.match(event.request)
-                .then(response => {
-                  if (response) {
-                    console.log('Found ', event.request.url, ' in cache');
-                    return response;
-                  }
-                  console.log('Network request for ', event.request.url);
-                  return fetch(event.request).then(response => {
-                    // TODO 5 - Respond with custom 404 page
-                    return caches.open(staticCacheName).then(cache => {
-                      cache.put(event.request.url, response.clone());
-                      return response;
-                    });
-                  });
-
-                }).catch(error => {
-
-                // TODO 6 - Respond with custom offline page
-
-              })
-            );
-          });
-
-
-          navigator.serviceWorker.addEventListener('activate', event => {
-            console.log('Activating new service worker...');
-
-            const cacheAllowlist = [staticCacheName];
-
-            event.waitUntil(
-              caches.keys().then(cacheNames => {
-                return Promise.all(
-                  cacheNames.map(cacheName => {
-                    if (cacheAllowlist.indexOf(cacheName) === -1) {
-                      return caches.delete(cacheName);
-                    }
-
-                    return Promise.resolve();
-                  })
-                );
-              })
-            );
-          });
-        }
 
         console.log('SW Reg found:', registration);
         if (registration.waiting) {
