1efe7836aa3888ff12127a5c07cb67dfab21c6cc
Alexander Fu <aafu@ucdavis.edu>
2020-10-02 02:37:21 -0400

    Remove MessagePack

--- src/v3/utils/protoUtils.ts
+++ src/v3/utils/protoUtils.ts
@@ -2,7 +2,6 @@
 import * as protobuf from 'protobufjs/light';
 import { toUint8Array } from 'v3/apps/GraphV3/libraries/SatisGraphtoryLib/sourcetools/base64';
 import * as LZUTF8 from 'lzutf8';
-import { decode } from '@msgpack/msgpack';
 import * as memoize from 'fast-memoize';
 
 const constrainedSchemas: any = {};
@@ -13,15 +12,14 @@ const protoSpecLoader = (version: string) => {
   const SGProtoData = protobufRoot.lookupType('satisgraphtory.SGProtoData');
 
   return fetch(`${process.env.PUBLIC_URL}/proto/${version}/data.s2`)
-    .then(data => data.json())
-    .then(data => data.d)
-    .then(data => toUint8Array(data))
-    .then(data => LZUTF8.decompress(data))
-    .then(data => toUint8Array(data))
-    .then(data => decode(data))
+    .then((data) => data.json())
+    .then((data) => data.d)
+    .then((data) => toUint8Array(data))
+    .then((data) => LZUTF8.decompress(data))
+    .then((data) => toUint8Array(data))
     .then((data: any) => {
       return SGProtoData.toObject(SGProtoData.fromObject(data), {
-        enums: String
+        enums: String,
       });
     });
 };
