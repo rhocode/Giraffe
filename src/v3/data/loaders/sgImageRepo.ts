16d6f89134accc672abfbf6799cbae7b2caf84fe
Alexander Fu <aafu@ucdavis.edu>
2020-11-23 05:23:52 -0500

    Add image caching

--- src/v3/data/loaders/sgImageRepo.ts
+++ src/v3/data/loaders/sgImageRepo.ts
@@ -7,27 +7,67 @@ export function getAllImageFiles() {
   return manifest.map((item: any) => cdn + item);
 }
 
-export function importImageManifest() {
+async function checkFile(
+  file: string,
+  cache: any,
+  refreshCache: boolean = false
+) {
+  const req = new Request(cdn + file);
   const promises = [];
+  if (!(await cache.match(req))) {
+    console.log('Adding image to cache:', file);
+    promises.push(cache.add(req));
+  }
+
+  return Promise.all(promises);
+}
+
+async function cacheImages() {
+  const cache = await caches.open('satisgraphtory-image-cache');
+  const promises: any = [];
+  const refreshCache = window.location.search.indexOf('refreshImages=1') > -1;
 
   for (const file of manifest) {
-    if (process.env.NODE_ENV === 'production') {
-      SGImageRepo.set(file, cdn + file);
-      promises.push(fetch(cdn + file, { cache: 'force-cache' }));
-    } else {
+    promises.push(checkFile(file, cache, refreshCache));
+  }
+
+  await Promise.all(promises);
+
+  const secondPromise = [];
+  for (const file of manifest) {
+    secondPromise.push(
+      cache
+        .match(cdn + file)
+        .then((resp) => {
+          if (!resp) {
+            throw new Error('Did not load properly');
+          }
+
+          return resp.blob();
+        })
+        .then((blob) => {
+          let imageUrl = URL.createObjectURL(blob);
+          SGImageRepo.set(file, imageUrl);
+          return Promise.resolve();
+        })
+    );
+  }
+  return await Promise.all(secondPromise);
+}
+
+export function importImageManifest() {
+  const promises: any = [];
+
+  if (process.env.NODE_ENV === 'production') {
+    promises.push(cacheImages());
+  } else {
+    for (const file of manifest) {
       const entryUrl = ((entries as unknown) as any)[
         'sg' + file.replace('.256.png', '_256').replace(/-/g, '_')
       ];
       SGImageRepo.set(file, entryUrl);
       promises.push(fetch(entryUrl, { cache: 'force-cache' }));
-      // console.log(entries)
     }
-
-    // promises.push(
-    //   Promise.resolve(() => {
-    //     return
-    //   })
-    // );
   }
 
   return Promise.all(promises);
